a857bd8d9a29ace20e1f3c5def0ece1f2a064a7e
ronze <ron.zeidman@gmail.com>
2016-01-06 22:58:58 +0200

    reproducing systemjs issue

---
+++ app/boot.js
@@ -0,0 +1,42 @@
+System.register(['angular2/bootstrap', 'angular2/core', 'angular2-jwt'], function(exports_1) {
+    "use strict";
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var bootstrap_1, core_1, angular2_jwt_1;
+    var AppComponent;
+    return {
+        setters:[
+            function (bootstrap_1_1) {
+                bootstrap_1 = bootstrap_1_1;
+            },
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (angular2_jwt_1_1) {
+                angular2_jwt_1 = angular2_jwt_1_1;
+            }],
+        execute: function() {
+            AppComponent = (function () {
+                function AppComponent() {
+                }
+                AppComponent = __decorate([
+                    core_1.Component({
+                        selector: 'my-app',
+                        template: '<p>loaded</p>'
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], AppComponent);
+                return AppComponent;
+            }());
+            bootstrap_1.bootstrap(AppComponent, [angular2_jwt_1.AuthHttp]);
+        }
+    }
+});
+//# sourceMappingURL=boot.js.map
